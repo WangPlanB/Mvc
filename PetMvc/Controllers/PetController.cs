ad662499a453ebff08cb80da1515d9531a7c2d14
至尊宝 <@>
2019-02-16 11:56:13 +0800

    模板页面

---
+++ PetMvc/Controllers/PetController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetMvc.Controllers
+{
+    public class PetController : Controller
+    {
+        // GET: Pet
+        public ActionResult Index()    // 主模板
+        {
+            return View();
+        }
+
+        public ActionResult Home()    // 系统首页
+        {
+            return View();
+        }
+
+        public ActionResult admin_info()   // 个人资料
+        {
+            return View();
+        }
+    }
+}
