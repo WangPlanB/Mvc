d4db2767dca0c21fb7f651c31cfe12d853ca96a7
宣布 <@DESKTOP-T9DFCI8>
2019-02-19 11:22:37 +0800

    users

---
+++ PetMvc/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using PetMvc.Models;
+namespace PetMvc.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(UsersModel model)
+        {
+            string str = JsonConvert.SerializeObject(model);
+            string reulst = HttpClientHelper.Send("post", "/api/UsersApi/", str);
+            return Content("111");
+        }
+    }
+}
